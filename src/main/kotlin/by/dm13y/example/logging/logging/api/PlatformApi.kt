ee594ffaedd6b5d6fb42d54040cccec7f2fb257f
DmitrievichD <d.dmitrievich@itransition.com>
2020-08-31 00:05:04 +0300

    add inner and external api

---
+++ src/main/kotlin/by/dm13y/example/logging/logging/api/PlatformApi.kt
@@ -0,0 +1,21 @@
+package by.dm13y.example.logging.logging.api
+
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
+/**
+ * Api for internal platform services
+ */
+@FeignClient(name = "inner-service-city", url = "https://openbank.demohoster.com")
+interface PlatformApi {
+
+    @GetMapping("/api/public/cities/{kladrId}")
+    fun getValidCity(@PathVariable(name = "kladrId") kladrId: String = "1900000100000")
+
+    @GetMapping("/api/public/cities/{kladrId}")
+    fun getInvalidCity(@PathVariable(name = "kladrId") kladrId: String = "0000000100000")
+
+    @GetMapping("/api/public/cities/some-url")
+    fun getUnknownUrl()
+}
