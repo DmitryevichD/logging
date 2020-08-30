1fba5098d7d715e567d0db5e556df21dbf88fb03
DmitrievichD <d.dmitrievich@itransition.com>
2020-08-31 00:24:13 +0300

    add implementation skeleton

---
+++ src/main/kotlin/by/dm13y/example/logging/logging/service/impl/ResourceServiceImpl.kt
@@ -0,0 +1,30 @@
+package by.dm13y.example.logging.logging.service.impl
+
+import by.dm13y.example.logging.logging.model.dto.ResourceDto
+import by.dm13y.example.logging.logging.service.ResourceService
+
+class ResourceServiceImpl : ResourceService {
+    override fun getPlatformValidResource(): ResourceDto {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPlatformInvalidResource(): ResourceDto {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPlatformUnknownResource(): ResourceDto {
+        TODO("Not yet implemented")
+    }
+
+    override fun getExternalValidResource(): ResourceDto {
+        TODO("Not yet implemented")
+    }
+
+    override fun getExternalInvalidResource(): ResourceDto {
+        TODO("Not yet implemented")
+    }
+
+    override fun getEternalUnknownResource(): ResourceDto {
+        TODO("Not yet implemented")
+    }
+}
