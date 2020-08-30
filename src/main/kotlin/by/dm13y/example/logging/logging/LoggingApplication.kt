c285cef5a80e956cf1dc13c788a6ff67e2d2948e
DmitrievichD <d.dmitrievich@itransition.com>
2020-08-30 23:34:17 +0300

    init

---
+++ src/main/kotlin/by/dm13y/example/logging/logging/LoggingApplication.kt
@@ -0,0 +1,11 @@
+package by.dm13y.example.logging.logging
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoggingApplication
+
+fun main(args: Array<String>) {
+    runApplication<LoggingApplication>(*args)
+}
---
+++ src/test/kotlin/by/dm13y/example/logging/logging/LoggingApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.dm13y.example.logging.logging
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoggingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
